2b6d78e9970d6e4ca39e9ef1e1223c5dfb267a71
Ilya Silkin <zerolar@gmail.com>
2021-06-06 18:22:37 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = new Exception[5];
+
+            exceptions[0] = new FileNotFoundException("Файл не существует.");
+            exceptions[1] = new DirectoryNotFoundException("Недопустимая часть пути к каталогу.");
+            exceptions[2] = new DivideByZeroException("Знаменатель в операции деления равен нулю.");
+            exceptions[3] = new DriveNotFoundException("Диск недоступен или не существует.");
+            exceptions[4] = new MyException("Собственный тип исключения.");
+
+            foreach (var item in exceptions)
+            {
+                try
+                {
+                    throw item;
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message) : base(message)
+        { }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberReader numberReader = new();
+            numberReader.NumberEnteredEvent += SortArray;
+
+            while (true)
+            {
+                try
+                {
+                    numberReader.Read();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введено некорректное значение: необходимо ввести число");
+                }
+                catch (MyException)
+                {
+                    Console.WriteLine("Введено некорректное значение: необходимо ввести либо 1, либо 2");
+                }
+            }
+            
+        }
+
+        static void SortArray (int number)
+        {
+            UsersData data = new();
+            var arr = data.Data;
+            
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Введено значение 1");
+                    Console.WriteLine();
+                    Array.Sort(arr);
+                    Console.WriteLine("Отсортированный массив: \n");
+                    foreach(var item in arr)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("Введено значение 2");
+                    Console.WriteLine();
+                    Array.Sort(arr);
+                    Array.Reverse(arr);
+                    Console.WriteLine("Отсортированный массив: \n");
+                    foreach (var item in arr)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    break;
+            }
+        }
+    }
+
+    public class UsersData
+    {
+        public string[] Data { get; set; }
+        public UsersData()
+        {
+            Data = new string[5]
+            {
+                    "Смирнов",
+                    "Иванов",
+                    "Кузнецов",
+                    "Соколов",
+                    "Попов"
+            };
+        }
+    }
+
+    class NumberReader
+    {
+        public delegate void NumberEnteredDelegate(int number);
+        public event NumberEnteredDelegate NumberEnteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Необходимо ввести значение: либо 1, либо 2");
+            Console.WriteLine("1 - сортировка от А до Я");
+            Console.WriteLine("2 - сортировка от Я до А");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2)
+            {
+                throw new MyException();
+            }
+
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEnteredEvent?.Invoke(number);
+        }
+    }
+
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message) : base(message)
+        { }
+    }
+}
